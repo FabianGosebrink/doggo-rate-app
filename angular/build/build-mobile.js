d86ad75c2c8ac87e1be6dac27e3c8e7442f22c67
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-11-10 07:50:05 +0100

    Adding mobiel assets

--- angular/build/build-mobile.js
+++ angular/build/build-mobile.js
@@ -1,19 +1,21 @@
-const shell = require("shelljs");
+const shell = require('shelljs');
 
-shell.echo("Start building mobile");
+shell.echo('Start building mobile');
 
-console.log("build angular");
-const angularBuildCommand = `ng build`;
+// npx cap add android
+
+console.log('build angular');
+const angularBuildCommand = `npm run build`;
 shell.exec(angularBuildCommand);
 
-console.log("adding splashscreen");
+console.log('adding splashscreen');
 const splashscreencommand = `npx capacitor-assets generate --android --assetPath assets/img`;
 shell.exec(splashscreencommand);
 
-console.log("Syncing app with capacitor");
+console.log('Syncing app with capacitor');
 const syncCommand = `npx cap sync android`;
 shell.exec(syncCommand);
 
-// console.log("Starting app with android");
+// console.log('Starting app with android');
 // const startCommand = `npx cap run android -- --verbose --sdk-info --stacktrace`;
 // shell.exec(startCommand);
--- angular/capacitor.config.ts
+++ angular/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'ratemydoggo',
-  webDir: 'dist/ratemydoggo',
+  webDir: 'dist/apps/doggo-rating-app',
   bundledWebRuntime: false,
 };
 

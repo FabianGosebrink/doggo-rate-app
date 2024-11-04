f53e510faa77e3767f0e1934ea3b896ef6119210
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-11-04 07:56:54 +0100

    Adding tests

--- angular/libs/doggos/feature/src/lib/main-doggo/main-doggo.component.spec.ts
+++ angular/libs/doggos/feature/src/lib/main-doggo/main-doggo.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainDoggoComponent } from './main-doggo.component';
+import { MockComponent, MockProvider } from 'ng-mocks';
+import { Doggo, DoggosStore } from '@doggo-rating/doggos/domain';
+import {
+  DoggoListComponent,
+  DoggoRateComponent,
+} from '@doggo-rating/doggos/ui';
+import { provideRouter } from '@angular/router';
+import { signal } from '@angular/core';
 
 describe('MainDoggoComponent', () => {
   let component: MainDoggoComponent;
@@ -8,16 +15,30 @@ describe('MainDoggoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainDoggoComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MainDoggoComponent,
+        MockComponent(DoggoListComponent),
+        MockComponent(DoggoRateComponent),
+      ],
+      providers: [
+        MockProvider(DoggosStore, {
+          loading: signal(false),
+          selectedDoggo: signal(null),
+          doggos: signal(new Array<Doggo>()),
+          loadDoggos: jest.fn(),
+          startListeningToRealtimeDoggoEvents: jest.fn(),
+          stopListeningToRealtimeDoggoEvents: jest.fn(),
+        }),
+        provideRouter([]),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MainDoggoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create ', () => {
     expect(component).toBeTruthy();
   });
 });

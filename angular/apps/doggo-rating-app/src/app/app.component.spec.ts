a94caed8c30bdca01f9983a2ab20932d333776fc
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-11-06 19:35:23 +0100

    Fixed app tests

--- angular/apps/doggo-rating-app/src/app/app.component.spec.ts
+++ angular/apps/doggo-rating-app/src/app/app.component.spec.ts
@@ -1,35 +1,29 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { MockProvider } from 'ng-mocks';
+import { AuthStore } from '@doggo-rating/shared/util-auth';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
+      imports: [AppComponent],
+      providers: [
+        MockProvider(AuthStore, {
+          checkAuth: jest.fn(),
+        }),
+        provideRouter([]),
       ],
     }).compileComponents();
+    
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ratemydoggo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ratemydoggo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ratemydoggo app is running!');
+    expect(component).toBeTruthy();
   });
 });
